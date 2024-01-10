e7ae1e3ea1699a664a214f874b9baabf3dc961f8
Your Name <you@example.com>
2024-01-10 10:15:08 +0200

    work 2/3

--- rps.py
+++ rps.py
@@ -9,6 +9,33 @@
 ROCK = "1"
 SCISSOR = "2"
 PAPER = "3"
-
+import random
 print("Welcome to Rock, Paper, Scissors!")
+print("Choose rock(1), peper(2) or Scissors(3)")
+my_choose = int(input("Your choose:"))
+computer_choose = (random.randint(1, 3))
+print ("My choose:" + str(computer_choose))
+if my_choose == 1 and computer_choose == 2:
+    print(" you win, you have rock, but me - scissors.")
+elif my_choose == 1 and computer_choose == 3:
+    print(" you lose, you have rock, but me - papar.")
+elif my_choose == 1 and computer_choose == 1:
+    print(" This is draw.")
+
+elif my_choose == 2 and computer_choose == 3:
+    print(" you win, you have scissors, but me - papar.")
+elif my_choose == 2 and computer_choose == 1:
+    print(" you lose, you have scissors, but me - rock.")
+elif my_choose == 2 and computer_choose == 2:
+    print(" This is draw.")
+
+elif my_choose == 3 and computer_choose == 1:
+    print(" you win, you have papar, but me - rock.")
+elif my_choose == 3 and computer_choose == 2:
+    print(" you lose, you have papar, but me - scissors.")
+elif my_choose == 3 and computer_choose == 3:
+    print(" This is draw.")
+print("Thunk you for game:)")
+
+
 
